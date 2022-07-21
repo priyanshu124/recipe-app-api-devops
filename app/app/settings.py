ad232a3a02bebebb05c3200d2cd4d20ca6ec92cd
priyanshu12498 <priyanshu.12498@gmail.comgit config --global user.name priyanshu12498git config --global user.email priyanshu.12498@gmail.com>
2022-07-22 01:17:27 +0530

    Fixed STATIC_URL

--- app/app/settings.py
+++ app/app/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/static/'
 MEDIA_URL = '/static/media/'
 
 MEDIA_ROOT = '/vol/web/media'
