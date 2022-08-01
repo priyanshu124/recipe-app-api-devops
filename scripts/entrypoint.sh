32e88955a1d79ac53469c6deb33fc95c2fbc8473
priyanshu12498 <priyanshu.12498@gmail.comgit config --global user.name priyanshu12498git config --global user.email priyanshu.12498@gmail.com>
2022-08-01 15:24:03 +0530

    Added Entry Point script

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
-
 python manage.py collectstatic --noinput
+python manage.py wait_for_db
+python manage.py migrate
+uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
 
-uswgi socket: 9000 --workers 4 --master --enable-threads --module app.wsgi
+# Run uWSGi as TCP/IP socket on port 9000.  We can map the requests from our proxy to this port 
