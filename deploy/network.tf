8c6049c6947569aca7d8b1b58431e3b8b141b577
priyanshu12498 <priyanshu.12498@gmail.comgit config --global user.name priyanshu12498git config --global user.email priyanshu.12498@gmail.com>
2022-08-18 15:44:22 +0530

    Added VPC and InternetGateway

--- deploy/network.tf
+++ deploy/network.tf
@@ -14,6 +14,6 @@ resource "aws_internet_gateway" "main" {
 
   tags = merge(
     local.common_tags,
-    { "Name" = "${local.prefix}-vpc" }
+    { "Name" = "${local.prefix}-internetGateway" }
   )
 }
