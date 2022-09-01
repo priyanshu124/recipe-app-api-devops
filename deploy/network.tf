1c93082a1bb21f0c53461dc35ee714422bfcd25b
priyanshu12498 <priyanshu.12498@gmail.comgit config --global user.name priyanshu12498git config --global user.email priyanshu.12498@gmail.com>
2022-09-01 14:03:15 +0530

    Added Private Subnets

--- deploy/network.tf
+++ deploy/network.tf
@@ -26,35 +26,36 @@ resource "aws_internet_gateway" "main" {
 # public_a #
 ############
 
-resource "aws_subnet" public_a{
-  cidr_block = "10.1.1.0/24"
+resource "aws_subnet" "public_a" {
+  cidr_block              = "10.1.1.0/24"
   map_public_ip_on_launch = true
-  vpc_id = aws_vpc.main.id 
-  availability_zone = "${data.aws_region.current}a"
+  vpc_id                  = aws_vpc.main.id
+  availability_zone       = "${data.aws_region.current.name}a"
 
   tags = merge(
     local.common_tags,
-    { "Name" = "${local.prefix}-public-a" }
+    {"Name" = "${local.prefix}-public-a"}
   )
 }
 
-resource "aws_route_table" "public_a"{
+resource "aws_route_table" "public_a" {
   vpc_id = aws_vpc.main.id
+
   tags = merge(
     local.common_tags,
-    { "Name" = "${local.prefix}-public-a" }
+    {"Name" = "${local.prefix}-public-a"}
   )
 }
 
-resource "aws_route_table_association" "public_a"{
-  subnet_id = aws_subnet.public_a.id
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
   route_table_id = aws_route_table.public_a.id
 }
 
 resource "aws_route" "public_internet_access_a" {
-  route_table_id = aws_route_table.public_a.id
-  destination_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.main.id
+  route_table_id         = aws_route_table.public_a.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
 }
 
 resource "aws_eip" "public_a" {
@@ -62,20 +63,19 @@ resource "aws_eip" "public_a" {
 
   tags = merge(
     local.common_tags,
-    { "Name" = "${local.prefix}-public-a" }
+    {"Name" = "${local.prefix}-public-a"}
   )
 }
 
-resource "aws_nat_gateway" "public_a"{
+resource "aws_nat_gateway" "public_a" {
   allocation_id = aws_eip.public_a.id
-  subnet_id = aws_subnet.public_a.id
+  subnet_id     = aws_subnet.public_a.id
 
   tags = merge(
     local.common_tags,
-    { "Name" = "${local.prefix}-public-a" }
+    {"Name" = "${local.prefix}-public-a"}
   )
 }
-
 
 
 ############
@@ -83,34 +83,35 @@ resource "aws_nat_gateway" "public_a"{
 ############
 
 resource "aws_subnet" "public_b" {
-  cidr_block = "10.1.2.0/24"
-  vpc_id = aws_vpc.main.id
+  cidr_block              = "10.1.2.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "${data.aws_region.current}b"
+  vpc_id                  = aws_vpc.main.id
+  availability_zone       = "${data.aws_region.current.name}b"
 
   tags = merge(
     local.common_tags,
-    { "Name" = "${local.prefix}-public-b" }
+    {"Name" = "${local.prefix}-public-b"}
   )
 }
 
-resource "aws_route_table" "public_b"{
+resource "aws_route_table" "public_b" {
   vpc_id = aws_vpc.main.id
+
   tags = merge(
     local.common_tags,
-    { "Name" = "${local.prefix}-public-b" }
+    {"Name" = "${local.prefix}-public-b"}
   )
 }
 
-resource "aws_route_table_association" "public_b"{
-  subnet_id = aws_subnet.public_b.id
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
   route_table_id = aws_route_table.public_b.id
 }
 
 resource "aws_route" "public_internet_access_b" {
-  route_table_id = aws_route_table.public_b.id
-  destination_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.main.id
+  route_table_id         = aws_route_table.public_b.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
 }
 
 resource "aws_eip" "public_b" {
@@ -118,16 +119,87 @@ resource "aws_eip" "public_b" {
 
   tags = merge(
     local.common_tags,
-    { "Name" = "${local.prefix}-public-b" }
+    {"Name" = "${local.prefix}-public-b"}
   )
 }
 
-resource "aws_nat_gateway" "public_b"{
+resource "aws_nat_gateway" "public_b" {
   allocation_id = aws_eip.public_b.id
-  subnet_id = aws_subnet.public_b.id
+  subnet_id     = aws_subnet.public_b.id
 
   tags = merge(
     local.common_tags,
-    { "Name" = "${local.prefix}-public-b" }
+    {"Name" = "${local.prefix}-public-b"}
   )
+}
+
+#################
+# Private Subnets 
+#################
+
+############
+# private_a #
+############
+
+resource "aws_subnet" "private_a" {
+  cidr_block        = "10.1.10.0/24"
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "${data.aws_region.current.name}a"
+
+  tags = merge(
+    local.common_tags,
+    {"Name" = "${local.prefix}-private-a"}
+  )
+}
+
+resource "aws_route_table" "private_a" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    {"Name" = "${local.prefix}-private-a"}
+  )
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private_a.id
+}
+
+resource "aws_route" "private_a_internet_out" {
+  route_table_id         = aws_route_table.private_a.id
+  nat_gateway_id         = aws_nat_gateway.public_a.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+
+resource "aws_subnet" "private_b" {
+  cidr_block        = "10.1.11.0/24"
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "${data.aws_region.current.name}b"
+
+  tags = merge(
+    local.common_tags,
+    {"Name" = "${local.prefix}-private-b"}
+  )
+}
+
+resource "aws_route_table" "private_b" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    {"Name" = "${local.prefix}-private-b"}
+  )
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private_b.id
+}
+
+resource "aws_route" "private_b_internet_out" {
+  route_table_id         = aws_route_table.private_b.id
+  nat_gateway_id         = aws_nat_gateway.public_b.id
+  destination_cidr_block = "0.0.0.0/0"
 }
