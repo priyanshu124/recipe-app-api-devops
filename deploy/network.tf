29813568c24bc81d61be1a46b089c5b959710a58
priyanshu12498 <priyanshu.12498@gmail.comgit config --global user.name priyanshu12498git config --global user.email priyanshu.12498@gmail.com>
2022-09-01 13:22:54 +0530

    Added vpc

--- deploy/network.tf
+++ deploy/network.tf
@@ -17,3 +17,117 @@ resource "aws_internet_gateway" "main" {
     { "Name" = "${local.prefix}-internetGateway" }
   )
 }
+
+####################################
+# Public Subnets - Internet Access #
+####################################
+
+############
+# public_a #
+############
+
+resource "aws_subnet" public_a{
+  cidr_block = "10.1.1.0/24"
+  map_public_ip_on_launch = true
+  vpc_id = aws_vpc.main.id 
+  availability_zone = "${data.aws_region.current}a"
+
+  tags = merge(
+    local.common_tags,
+    { "Name" = "${local.prefix}-public-a" }
+  )
+}
+
+resource "aws_route_table" "public_a"{
+  vpc_id = aws_vpc.main.id
+  tags = merge(
+    local.common_tags,
+    { "Name" = "${local.prefix}-public-a" }
+  )
+}
+
+resource "aws_route_table_association" "public_a"{
+  subnet_id = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public_a.id
+}
+
+resource "aws_route" "public_internet_access_a" {
+  route_table_id = aws_route_table.public_a.id
+  destination_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.main.id
+}
+
+resource "aws_eip" "public_a" {
+  vpc = true
+
+  tags = merge(
+    local.common_tags,
+    { "Name" = "${local.prefix}-public-a" }
+  )
+}
+
+resource "aws_nat_gateway" "public_a"{
+  allocation_id = aws_eip.public_a.id
+  subnet_id = aws_subnet.public_a.id
+
+  tags = merge(
+    local.common_tags,
+    { "Name" = "${local.prefix}-public-a" }
+  )
+}
+
+
+
+############
+# public_b #
+############
+
+resource "aws_subnet" "public_b" {
+  cidr_block = "10.1.2.0/24"
+  vpc_id = aws_vpc.main.id
+  map_public_ip_on_launch = true
+  availability_zone = "${data.aws_region.current}b"
+
+  tags = merge(
+    local.common_tags,
+    { "Name" = "${local.prefix}-public-b" }
+  )
+}
+
+resource "aws_route_table" "public_b"{
+  vpc_id = aws_vpc.main.id
+  tags = merge(
+    local.common_tags,
+    { "Name" = "${local.prefix}-public-b" }
+  )
+}
+
+resource "aws_route_table_association" "public_b"{
+  subnet_id = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public_b.id
+}
+
+resource "aws_route" "public_internet_access_b" {
+  route_table_id = aws_route_table.public_b.id
+  destination_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.main.id
+}
+
+resource "aws_eip" "public_b" {
+  vpc = true
+
+  tags = merge(
+    local.common_tags,
+    { "Name" = "${local.prefix}-public-b" }
+  )
+}
+
+resource "aws_nat_gateway" "public_b"{
+  allocation_id = aws_eip.public_b.id
+  subnet_id = aws_subnet.public_b.id
+
+  tags = merge(
+    local.common_tags,
+    { "Name" = "${local.prefix}-public-b" }
+  )
+}
