4b2b63078df519c30cfe15125761abe4304b8f84
root <root@LAPTOP-K8JGE4PG.localdomain>
2022-11-06 17:39:03 +0530

    Added instance profile for Bastion

--- deploy/bastion.tf
+++ deploy/bastion.tf
@@ -7,10 +7,27 @@ data "aws_ami" "amazon_linux" {
   owners = ["137112412989"]
 }
 
+resource "aws_iam_role" "bastion" {
+  name               = "${local.prefix}-bastion"
+  assume_role_policy = file("./template/bastion/instance-profile-policy.json")
+  tags               = local.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "bastion_attach_policy" {
+  role       = aws_iam_role.bastion.name
+  policy_arn = "arn:aws:iam:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_instance_profile" "bastion" {
+  name = "${local.prefix}-bastion-instance-profile"
+  role = aws_iam_role.bastion.name
+}
+
 resource "aws_instance" "bastion" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t2.micro"
-  user_data     = file("./template/bastion/user-data.sh")
+  ami                  = data.aws_ami.amazon_linux.id
+  instance_type        = "t2.micro"
+  user_data            = file("./template/bastion/user-data.sh")
+  iam_instance_profile = aws_iam_instance_profile.bastion.name
 
   tags = merge(
     local.common_tags,
